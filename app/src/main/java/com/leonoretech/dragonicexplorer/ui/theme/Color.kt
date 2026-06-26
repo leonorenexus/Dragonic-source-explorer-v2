8fe78034137662a4e785cbd483def1260c8c3a63
Leonorenexus <leonorenexus@gmail.com>
2026-06-26 15:45:04 +0700

    Create Color.kt

---
+++ app/src/main/java/com/leonoretech/dragonicexplorer/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.leonoretech.dragonicexplorer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Base surfaces
+val DeepSpaceBlack = Color(0xFF050810)
+val SurfaceDark = Color(0xFF0A0F1C)
+val SurfaceElevated = Color(0xFF0F1626)
+val CardBorder = Color(0xFF1A2438)
+
+// Neon accents (Leonore Tech Team signature palette)
+val NeonCyan = Color(0xFF22D3EE)
+val NeonCyanBright = Color(0xFF67E8F9)
+val NeonMagenta = Color(0xFFE344F0)
+val NeonBlue = Color(0xFF3B82F6)
+
+// Status colors
+val SuccessGreen = Color(0xFF34D399)
+val FailedRed = Color(0xFFF87171)
+val RunningBlue = Color(0xFF60A5FA)
+val WarningAmber = Color(0xFFFBBF24)
+
+// Text
+val TextPrimary = Color(0xFFE6F1FF)
+val TextSecondary = Color(0xFF7C8BA8)
+val TextMuted = Color(0xFF4B5876)
